9c7185b731e0cafb2af73edac3dcbcd3299f523f
SOO <aubepluieh3@naver.com>
2022-07-01 14:55:24 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, {Component} from "react";
-import MyComponent from "./MyComponent";
+import MyComponent from "./MyComponent"; //MyComponent 파일을 불러옵니다.
 class App extends Component{
   render(){
     return (
-      <MyComponent/>
+      <MyComponent name = "React"/>
     );
   }
 
