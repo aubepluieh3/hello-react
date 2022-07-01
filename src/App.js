07a15b3f9103b3107e9c4cf8e903ea430080ffba
SOO <aubepluieh3@naver.com>
2022-07-01 14:58:09 +0900

    defaultProps

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import MyComponent from "./MyComponent"; //MyComponent 파일을 불러옵니다
 class App extends Component{
   render(){
     return (
-      <MyComponent name = "React"/>
+      <MyComponent/>
     );
   }
 
